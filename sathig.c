92c29e040cd2e872e5fe2ab8751cf684293984c2
sakthigowri01 <35915299+sakthigowri01@users.noreply.github.com>
2018-03-14 15:57:50 +0530

    sathig.c

---
+++ sathig.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a[14],sum=0,n,i;
+scanf("%d",n);
+for(i=0;i<n;i++)
+{
+ scanf("%d",a[j]);
+ }
+ for(i=0;i<n;i++)
+ {
+ sum=sum+a[j];
+ }
+ printf("sum is %d",sum);
+ }
