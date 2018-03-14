428f824ef020802d3dee3625eebd65989b4de071
sakthigowri01 <35915299+sakthigowri01@users.noreply.github.com>
2018-03-14 16:16:16 +0530

    gowrisakthi.c

---
+++ gowrisakthi.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) {
+	// your code goes here
+	int n,p,r,si;
+	scanf("%d %d %d",&p,&n,&r);
+	si=(p*n*r)/100;
+	printf("%d",si);
+	return 0;
+}
+© 2018 GitHub, Inc.
